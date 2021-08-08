deebfd23252946fb7a00766ee6f192b7a407fb61
Breanne Sullivan <breannecsullivan@gmail.com>
2021-08-08 12:18:44 -0400

    Bootstrap Final Submission

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+function hoverAmericas(isMouseOver) {
+    if (isMouseOver){
+        $("#americasPhoto").attr("src", "../img/argentina.jpg")  
+    } else {
+        $("#americasPhoto").attr("src", "../img/peruport.jpg")
+    }
+}
+
+function hoverOceania(isMouseOver) {
+    if (isMouseOver) {
+        $("#oceaniaPhoto").attr("src", "../img/nz2.jpg")
+    } else{
+        $("#oceaniaPhoto").attr("src", "../img/sydneyport.jpg")
+    }
+}
+
+function hoverAsia(isMouseOver) {
+    if (isMouseOver) {
+        $("#asiaPhoto").attr("src", "../img/ubudmonkey.jpg")
+    } else{
+        $("#asiaPhoto").attr("src", "../img/japanport.jpg")
+    }
+}
+
+function hoverEurope(isMouseOver) {
+    if (isMouseOver) {
+        $("#europePhoto").attr("src", "../img/copenhagen2.jpg")
+    } else{
+        $("#europePhoto").attr("src", "../img/romeport.jpg")
+    }
+}
+
+function hoverAfrica(isMouseOver) {
+    if (isMouseOver) {
+        $("#africaPhoto").attr("src", "../img/elephanttanz.jpg")
+    } else{
+        $("#africaPhoto").attr("src", "../img/tanzaniaport.jpg")
+    }
+}
+
+function hoverAntarctica(isMouseOver) {
+    if (isMouseOver) {
+        $("#antarcticaPhoto").attr("src", "../img/antarcticap.jpg")
+    } else{
+        $("#antarcticaPhoto").attr("src", "../img/antarcticafolio.jpg")
+    }
+}
+
+
+
+
+
+
